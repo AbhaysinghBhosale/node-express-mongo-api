3e46b3b0af012bdbbfa3d23d30b97e99cfbe4127
abhayb <Abhayb@cloudneeti.com>
2019-07-31 14:47:28 +0530

    Added validation to the post using the express validator

--- validator/index.js
+++ validator/index.js
@@ -1,10 +1,10 @@
 exports.createPostValidator = (req,res,next)=>{
     //validate title
-    req.check('title','Please enter post title').nonEmpty();
+    req.check('title','Please enter post title').notEmpty();
     req.check('title','Post title must be min 10 and max 150 characters').isLength({min:10,max:150});
 
     //validate body
-    req.check('body','Please enter body title').nonEmpty();
+    req.check('body','Please enter post body').notEmpty();
     req.check('body','Post body must be min 4 and max 2000 characters').isLength({min:4,max:2000});
 
     const errors = req.validationErrors();
